593140ab6eda2836cdbaf7f936574154c9031480
Denis <demaz@bk.ru>
2023-05-05 10:17:12 +0700

    - added mapper for ResponseWrapperAdsDto

--- src/main/java/ru/skypro/homework/mapper/ResponseWrapperAdsDtoMapper.java
+++ src/main/java/ru/skypro/homework/mapper/ResponseWrapperAdsDtoMapper.java
@@ -1,7 +1,23 @@
 package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import ru.skypro.homework.dto.ResponseWrapperAdsDto;
+import ru.skypro.homework.entity.Ads;
 
-@Mapper
+import java.util.List;
+
+@Mapper(uses = AdsDtoMapper.class)
 public interface ResponseWrapperAdsDtoMapper {
+
+    ResponseWrapperAdsDtoMapper INSTANCE = Mappers.getMapper(ResponseWrapperAdsDtoMapper.class);
+
+    default ResponseWrapperAdsDto toResponseWrapperAdsDto(List<Ads> ads) {
+        return toResponseWrapperAdsDto(ads.size(), ads);
+    }
+
+    @Mapping(target = "results", source = "ads")
+    @Mapping(target = "count", source = "count")
+    ResponseWrapperAdsDto toResponseWrapperAdsDto(Integer count, List<Ads> ads);
 }
