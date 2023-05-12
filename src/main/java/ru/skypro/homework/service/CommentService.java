7355d8c6df120da13c5ba255ebdbb46a9c9a0628
Denis <demaz@bk.ru>
2023-05-12 17:39:17 +0700

    - added documentation

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -15,6 +15,9 @@ import ru.skypro.homework.repository.UserRepository;
 
 import java.time.LocalDateTime;
 
+/**
+ * Сервис для работы с сущностью {@link Comment}.
+ */
 @Slf4j
 @Service
 public class CommentService {
@@ -29,11 +32,26 @@ public class CommentService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Метод принимает id объявления, получает список всех комментариев к данному объявлению и возвращает ответ в виде DTO.
+     *
+     * @param adId идентификатор объявления
+     * @return {@link ResponseWrapperCommentDto}
+     */
     public ResponseWrapperCommentDto getAdComments(int adId) {
         log.info("Was invoked method - getAdComments");
         return ResponseWrapperCommentDtoMapper.INSTANCE.toResponseWrapperCommentDto(commentRepository.findByAds_Id(adId));
     }
 
+    /**
+     * Метод принимает id объявления для которого создается комментарий и необходимые данные для создания комментария,
+     * в виде DTO. На их основе создает сущность {@link Comment} и сохраняет ее в БД.
+     * После сохранения возвращает актуальные данные о созданном комментарии в виде DTO.
+     *
+     * @param adId           идентификатор объявления для которого создается комментарий
+     * @param createdComment необходимые данные для создания комментария
+     * @return {@link CommentDto}
+     */
     public CommentDto addCommentToAd(int adId, CreateCommentDto createdComment) {
         log.info("Was invoked method - addCommentToAd");
 
@@ -47,21 +65,37 @@ public class CommentService {
         return CommentMapper.INSTANCE.commentToCommentDto(savedComment);
     }
 
+    /**
+     * Метод принимает id комментария, по id удаляет сущность {@link Comment} из БД.
+     *
+     * @param commentId идентификатор комментария
+     */
     public void deleteAdsComment(int commentId) {
         log.info("Was invoked method - deleteAdsComment");
         commentRepository.deleteById(commentId);
     }
 
+    /**
+     * Метод принимает id комментария и его актуальные данные, ищет сущность {@link Comment} в БД,
+     * если комментарий не найден - возвращает {@code null},
+     * если найден - изменяет данные на актуальные и возвращает новые данные комментария в виде DTO.
+     *
+     * @param commentId идентификатор комментария
+     * @param newData   новые данные для изменения комментария
+     * @return {@link CommentDto} / {@code null}
+     */
     public CommentDto updateAdComment(int commentId, CommentDto newData) {
         log.info("Was invoked method - updateAdComment");
 
-        Comment updatedComment = commentRepository.findById(commentId);
-        if (updatedComment == null) {
+        Comment oldCommentData = commentRepository.findById(commentId);
+        if (oldCommentData == null) {
             return null;
         }
 
-        updatedComment.setText(newData.getText());
-        commentRepository.save(updatedComment);
+        oldCommentData.setText(newData.getText());
+
+        Comment updatedComment = commentRepository.save(oldCommentData);
+
         return CommentMapper.INSTANCE.commentToCommentDto(updatedComment);
     }
 }
