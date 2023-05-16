44a9b3abf7d68d13685d9d42d1a69abcdebef2ba
xandrosanchez <din.myrk@gmail.com>
2023-05-17 00:50:39 +0300

    add tests for UserService

--- src/test/java/ru/skypro/homework/service/UserServiceTest.java
+++ src/test/java/ru/skypro/homework/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import ru.skypro.homework.dto.NewPasswordDto;
+import ru.skypro.homework.dto.UserDto;
 import ru.skypro.homework.entity.User;
 import ru.skypro.homework.repository.UserRepository;
 
@@ -37,7 +38,7 @@ public class UserServiceTest {
         User currentUser = new User();
         currentUser.setPassword("oldPassword");
 
-        when(userRepository.save(currentUser)).thenReturn(currentUser);
+        when(userRepository.save(any(User.class))).thenReturn(currentUser);
         when(userRepository.findByEmail(anyString())).thenReturn(currentUser);
 
         NewPasswordDto newPasswordDto = new NewPasswordDto();
@@ -51,7 +52,39 @@ public class UserServiceTest {
     }
 
     @Test
-    void getMe() {
+    public void testSetPassword_Failure() {
+        User currentUser = new User();
+        currentUser.setPassword("oldPassword");
+
+        when(userRepository.save(any(User.class))).thenReturn(currentUser);
+        when(userRepository.findByEmail(anyString())).thenReturn(currentUser);
+
+        NewPasswordDto newPasswordDto = new NewPasswordDto();
+        newPasswordDto.setCurrentPassword("wrongPassword");
+        newPasswordDto.setNewPassword("newPassword");
+
+        boolean result = userService.setPassword(newPasswordDto);
+
+        assertFalse(result);
+        assertEquals("oldPassword", currentUser.getPassword());
+    }
+
+    @Test
+    public void testGetMe() {
+        User currentUser = new User();
+        currentUser.setEmail("test@example.com");
+        currentUser.setFirstName("John");
+        currentUser.setLastName("Doe");
+        currentUser.setPhone("1234567890");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(currentUser);
+
+        UserDto userDto = userService.getMe();
+
+        assertNotNull(userDto);
+        assertEquals("John", userDto.getFirstName());
+        assertEquals("Doe", userDto.getLastName());
+        assertEquals("1234567890", userDto.getPhone());
     }
 
     @Test
