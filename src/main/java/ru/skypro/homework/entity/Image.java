88db0cb426ed2903c86c5971fc0c6860cf7f7375
Denis <demaz@bk.ru>
2023-05-22 17:07:03 +0700

    - added image entity and table for it

---
+++ src/main/java/ru/skypro/homework/entity/Image.java
@@ -0,0 +1,26 @@
+package ru.skypro.homework.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String filePath;
+
+    @OneToOne
+    @JoinColumn(name = "ad_id")
+    private Ads ad;
+
+    public String getImagePath() {
+        return "/image/" + this.ad.getId();
+    }
+}
