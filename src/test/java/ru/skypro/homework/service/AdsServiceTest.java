d52495f31f8cc163985ac868a85af36644fa0b1f
xandrosanchez <din.myrk@gmail.com>
2023-05-22 05:42:42 +0300

    Добавил несколько тестов для Ads

--- src/test/java/ru/skypro/homework/service/AdsServiceTest.java
+++ src/test/java/ru/skypro/homework/service/AdsServiceTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import ru.skypro.homework.dto.FullAdsDto;
-import ru.skypro.homework.dto.ResponseWrapperAdsDto;
-import ru.skypro.homework.dto.Role;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.skypro.homework.dto.*;
 import ru.skypro.homework.entity.Ads;
 import ru.skypro.homework.entity.User;
 import ru.skypro.homework.repository.AdsRepository;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -33,6 +35,9 @@ public class AdsServiceTest {
     public void setup() {
         MockitoAnnotations.openMocks(this);
         adsService = new AdsService(adsRepository, userRepository);
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken("test@example.com", null));
+        SecurityContextHolder.setContext(securityContext);
     }
 
     @Test
@@ -98,6 +103,75 @@ public class AdsServiceTest {
         assertEquals(fullAdsDtoExpected,fullAdsDto);
     }
 
+    @Test
+    public void testUpdateAd_Success(){
+        User author = new User();
+        author.setId(1);
+        author.setEmail("john.doe@example.com");
+        author.setFirstName("John");
+        author.setLastName("Doe");
+        author.setPhone("1234567890");
+        author.setImage("path/to/user/image.jpg");
+        author.setPassword("password123");
+        author.setRole(Role.USER);
 
+        Ads oldAdData = new Ads();
+        oldAdData.setId(1);
+        oldAdData.setPrice(1000);
+        oldAdData.setDescription("This is a great product.");
+        oldAdData.setTitle("Awesome Product");
+        oldAdData.setAuthor(author);
+        oldAdData.setImage("path/to/ad/image.jpg");
+
+        CreateAdsDto newAdData = new CreateAdsDto();
+        newAdData.setDescription("New description");
+        newAdData.setPrice(500);
+        newAdData.setTitle("New Title");
+
+        Ads updatedAd = oldAdData;
+        updatedAd.setDescription(newAdData.getDescription());
+        updatedAd.setPrice(newAdData.getPrice());
+        updatedAd.setTitle(newAdData.getTitle());
+
+        AdsDto adsDtoExpected = new AdsDto();
+        adsDtoExpected.setAuthor(1);
+        adsDtoExpected.setImage("path/to/ad/image.jpg");
+        adsDtoExpected.setPk(1);
+        adsDtoExpected.setPrice(500);
+        adsDtoExpected.setTitle("New Title");
+
+        when(adsRepository.save(oldAdData)).thenReturn(updatedAd);
+        when(adsRepository.findById(1)).thenReturn(oldAdData);
+
+        AdsDto adsDto = adsService.updateAd(1,newAdData);
+
+        assertNotNull(adsDto);
+        assertEquals(adsDtoExpected,adsDto);
+    }
+
+    @Test
+    public void testUpdateAd_Null(){
+        CreateAdsDto newAdData = new CreateAdsDto();
+        newAdData.setDescription("New description");
+        newAdData.setPrice(500);
+        newAdData.setTitle("New Title");
+
+        when(adsRepository.findById(1)).thenReturn(null);
+
+        AdsDto adsDto = adsService.updateAd(1,newAdData);
+
+        assertNull(adsDto);
+    }
+
+    @Test
+    public void testGetMyAds(){
+        List<Ads> adsList = new ArrayList<>();
+
+        when(adsRepository.findAllByAuthor(any())).thenReturn(adsList);
+
+        ResponseWrapperAdsDto responseWrapperAdsDto = adsService.getMyAds();
+
+        assertNotNull(responseWrapperAdsDto);
+    }
 }
 
