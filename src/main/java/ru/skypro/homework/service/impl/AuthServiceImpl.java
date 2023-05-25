656f7c25344f6aa5b5e88aa1ed7b95c8b67ecb65
Denis <demaz@bk.ru>
2023-05-26 01:42:31 +0700

    - added documentation

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.skypro.homework.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,21 +13,27 @@ import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.security.UserDetailManagerImpl;
 import ru.skypro.homework.service.AuthService;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
     private final UserDetailManagerImpl manager;
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
 
-    public AuthServiceImpl(UserDetailManagerImpl manager, UserRepository userRepository, PasswordEncoder encoder) {
-        this.manager = manager;
-        this.userRepository = userRepository;
-        this.encoder = encoder;
-    }
-
+    /**
+     * Метод проверяет есть ли пользователь в БД, если пользователь не найден в БД возвращает {@code false},
+     * если пользователь найден получает его из БД, возвращает результат сравнения пароля переданного
+     * в параметрах метода и пароля пользователя, используя {@link PasswordEncoder}.
+     *
+     * @param userName имя пользователя
+     * @param password пароль введенный пользователем
+     * @return {@code true}/{@code false}
+     */
     @Override
     public boolean login(String userName, String password) {
+        log.info("Was invoked method - login");
         if (!manager.userExists(userName)) {
             return false;
         }
@@ -33,18 +41,36 @@ public class AuthServiceImpl implements AuthService {
         return encoder.matches(password, userDetails.getPassword());
     }
 
+    /**
+     * Метод проверяет есть ли пользователь в БД, если пользователь не найден в БД возвращает {@code false},
+     * если пользователь найден, сохраняет его в БД и возвращает {@code true}.
+     *
+     * @param registerData данные для регистрации пользователя в виде DTO
+     * @param role роль пользователя {@link Role}
+     * @return {@code true}/{@code false}
+     */
     @Override
-    public boolean register(RegisterReq registerReq, Role role) {
-        if (manager.userExists(registerReq.getUsername())) {
+    public boolean register(RegisterReq registerData, Role role) {
+        log.info("Was invoked method - register");
+        if (manager.userExists(registerData.getUsername())) {
             return false;
         }
-        saveUserInDb(registerReq, role);
+        saveUserInDb(registerData, role);
         return true;
     }
 
-    private void saveUserInDb(RegisterReq registerReq, Role role) {
-        User newUser = UserMapper.INSTANCE.registerReqToUser(registerReq);
-        newUser.setPassword(encoder.encode(registerReq.getPassword()));
+    /**
+     * Метод создает сущность {@link User} на основании регистрационных данных,
+     * устанавливает пароль с помощью {@link PasswordEncoder}, устанавливает переданную роль,
+     * сохраняет пользователя в БД.
+     *
+     * @param registerData данные для регистрации пользователя в виде DTO
+     * @param role роль пользователя
+     */
+    private void saveUserInDb(RegisterReq registerData, Role role) {
+        log.info("Was invoked method - saveUserInDb");
+        User newUser = UserMapper.INSTANCE.registerReqToUser(registerData);
+        newUser.setPassword(encoder.encode(registerData.getPassword()));
         newUser.setRole(role);
         userRepository.save(newUser);
     }
